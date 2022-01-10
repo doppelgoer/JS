cc02e6ad8b8de6b1455e156bea544eb2dcf51946
장준영 <doppelgoer1@naver.com>
2022-01-10 21:48:46 +0900

    push

---
+++ design pattern/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+let mysql = require("mysql");
+const Singleton = require("./singleton");
+let connection = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "112213",
+  database: "poko", // 데이터베이스 고르기
+  port: "3306",
+});
+console.log(111);
+const test = Singleton.getInstance();
+test.test = 11;
+const test1 = Singleton.getInstance();
+console.log(test);
+console.log(test1);
+console.log(test.databaseConnection);
+console.log(test1.databaseConnection);
---
+++ design pattern/singleton.js
@@ -0,0 +1,23 @@
+class DatabaseConnection {
+  constructor() {
+    this.databaseConnection = "dummytext";
+  }
+
+  getNewDBConnection() {
+    return this.databaseConnection;
+  }
+}
+class Singleton {
+  constructor() {
+    throw new Error("Use singleton.getInstance()");
+  }
+  static getInstance() {
+    if (!Singleton.instance) {
+      Singleton.instance = new DatabaseConnection();
+    }
+
+    return Singleton.instance;
+  }
+}
+
+module.exports = Singleton;
