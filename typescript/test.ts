40a96f517f420920becea7519cb25afb00cc611b
장준영 <doppelgoer1@naver.com>
2022-03-30 00:51:22 +0900

    typescript start

--- typescript/test.ts
+++ typescript/test.ts
@@ -1,6 +1,23 @@
-function printLabel(labeledObj: { label: string }) {
-  console.log(labeledObj.label);
+// function printLabel(labeledObj: { label: string }) {
+//   console.log(labeledObj.label);
+// }
+
+// let myObj = { size: 10, label: "Size 10 Object" };
+// printLabel(myObj);
+
+interface Test {
+  name: string;
+  age: number;
 }
 
-let myObj = { size: 10, label: "Size 10 Object" };
-printLabel(myObj);
+class Member {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+let 준영: Test = new Member("준영", 29);
+console.log(준영);
