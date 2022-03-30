adfe32aad0eafecfc82814b749cf38ea12404946
장준영 <doppelgoer1@naver.com>
2022-03-31 02:26:49 +0900

    'class퍼블릭,스태틱,프라이빗'

--- typescript/test.js
+++ typescript/test.js
@@ -1,5 +1,22 @@
 function printLabel(labeledObj) {
-    console.log(labeledObj.label);
+  console.log(labeledObj.label);
 }
 var myObj = { size: 10, label: "Size 10 Object" };
 printLabel(myObj);
+
+class Student {
+  #name;
+  math;
+  english;
+  science;
+
+  constructor(name, math, english, science) {
+    this.#name = name;
+    this.math = math;
+    this.english = english;
+    this.science = science;
+  }
+  get name() {
+    return this._name;
+  }
+}
